4b0c10202c36e0788b8a62bc5cddbf80adbc13e6
anastasiosgrigoriou <agrigori@mail.sfsu.edu>
2018-03-08 19:26:24 -0800

    Initial Commit

---
+++ FindAGrave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindAGrave
+//
+//  Created by Anastasios Grigoriou on 3/8/18.
+//  Copyright © 2018 Grigoriou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
