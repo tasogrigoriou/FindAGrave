6247ec40ece783d54c00e6d3e4cf49ce1d869db3
anastasiosgrigoriou <agrigori@mail.sfsu.edu>
2018-03-09 10:17:41 -0800

    CemeterySummary implementation

--- FindAGrave/ViewController.swift
+++ FindAGrave/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+   var cemeteries = [CemeterySummary]()
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+      
+      getCemetery("15559")
    }
 
    override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
       // Dispose of any resources that can be recreated.
    }
-
-
+   
+   func getCemetery(_ idNumber: String) {
+      Cemetery.cemeteryById(idNumber, completionHandler: { (cemetery, error) in
+         if let error = error {
+            print(error)
+            return
+         }
+         guard let cemetery = cemetery else {
+            print("error getting cemetery: result is nil")
+            return
+         }
+         // success
+         debugPrint(cemetery)
+         print(cemetery)
+         
+         self.cemeteries.append(cemetery.cemeterySummary)
+      })
+   }
+   
 }
+
+enum BackendError: Error {
+   case urlError(reason: String)
+   case objectSerialization(reason: String)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
